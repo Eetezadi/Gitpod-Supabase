882640f7a670347d33e3fac04cea50213a4a8a56
Sina Eetezadi <code@eetezadi.com>
2023-11-28 16:24:33 +0000

    functions and test case

---
+++ hello-world-test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Sources the variables of the .env file
+source .env
+
+# API call to the hello-world edge function
+curl --request POST 'http://localhost:54321/functions/v1/hello-world' \
+  --header "Authorization: Bearer $SUPABASE_ANON_KEY" \
+  --header 'Content-Type: application/json' \
+  --data '{ "name":"Gitpod User!" }'
