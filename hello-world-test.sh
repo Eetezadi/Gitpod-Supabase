43d52dceabea8a875df7b5b80271e7b4350becd5
Sina Eetezadi <code@eetezadi.com>
2024-09-19 12:02:06 +0000

    remove link, better with dialog

--- hello-world-test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# API call to the hello-world edge function
-curl --request POST 'http://localhost:54321/functions/v1/hello-world' \
-  --header "Authorization: Bearer $SUPABASE_ANON_KEY" \
-  --header 'Content-Type: application/json' \
-  --data '{ "name":"Gitpod User!" }'
